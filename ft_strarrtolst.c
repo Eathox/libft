ba347a9a814f03938015a2f3e21e975f7617c922
pholster <pholster@student.codam.nl>
2019-01-31 18:50:35 +0100

    fixed nrom

--- ft_strarrtolst.c
+++ ft_strarrtolst.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/13 14:48:15 by pholster       #+#    #+#                */
-/*   Updated: 2019/01/31 17:51:56 by pholster      ########   odam.nl         */
+/*   Updated: 2019/01/31 18:49:05 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,23 @@ t_list	*ft_strarrtolst(char **stra)
 	t_list	*prvlst;
 	t_list	*returnlst;
 	int		i;
-	int		j;
 
 	i = 0;
-	j = 0;
 	if (stra == NULL)
 		return (NULL);
 	returnlst = ft_lstnew(stra[i], ft_strlen(stra[i]) + 1);
 	if (returnlst == NULL)
 		return (NULL);
 	prvlst = returnlst;
-	while (stra[i + 1] != 0)
+	i++;
+	while (stra[i] != 0)
 	{
-		i++;
 		lst = ft_lstnew(stra[i], ft_strlen(stra[i]) + 1);
 		if (lst == NULL)
 			return (NULL);
 		ft_lstaddbck(&prvlst, lst);
 		prvlst = lst;
+		i++;
 	}
 	return (returnlst);
 }
