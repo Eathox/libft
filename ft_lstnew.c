fadcefddcf677e07c6b73f11257f182b370dc5af
pholster <pholster@student.codam.nl>
2019-02-06 03:18:22 +0100

    added memory leak preventions

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,13 +6,19 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/11 18:07:37 by pholster       #+#    #+#                */
-/*   Updated: 2019/02/04 20:46:27 by pholster      ########   odam.nl         */
+/*   Updated: 2019/02/06 03:17:39 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void const *content, size_t content_size)
+static t_list	*freeret(t_list *lst)
+{
+	free(lst);
+	return (NULL);
+}
+
+t_list			*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*lst;
 
@@ -28,7 +34,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	{
 		lst->content = ft_memalloc(content_size);
 		if (lst->content == NULL)
-			return (NULL);
+			return (freeret(lst));
 		ft_memcpy(lst->content, content, content_size);
 		lst->content_size = content_size;
 	}
