09c87dbe35f246442132a7fe302700a0cce1fae9
pholster <pholster@student.codam.nl>
2019-02-10 08:36:23 +0100

    updated pow

--- ft_pow.c
+++ ft_pow.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/10 17:13:48 by pholster       #+#    #+#                */
-/*   Updated: 2019/02/09 12:41:19 by pholster      ########   odam.nl         */
+/*   Updated: 2019/02/10 08:36:06 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 long	ft_pow(long n, long pow)
 {
-	long	r;
+	long	ret;
 
-	r = n;
+	ret = n;
 	if (pow == 0)
 		return (1);
 	if (pow == 1 && n < 0)
@@ -25,14 +25,12 @@ long	ft_pow(long n, long pow)
 		return (0);
 	if (n < 0)
 		n = -n;
-	if (r < 0)
-		r = -r;
+	if (ret < 0)
+		ret = -ret;
 	while (pow > 1)
 	{
 		pow--;
-		r = r * n;
+		ret *= n;
 	}
-	if (r > 2147483647)
-		return (1);
-	return ((int)r);
+	return (ret);
 }
