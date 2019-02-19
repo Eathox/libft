2221319af13b79e33037a289bde71c251843fd39
pholster <pholster@student.codam.nl>
2019-02-19 09:33:25 +0100

    WIP getchar that gets trough norm

--- ft_getchar.c
+++ ft_getchar.c
@@ -6,11 +6,31 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/02/18 17:47:13 by pholster       #+#    #+#                */
-/*   Updated: 2019/02/19 09:32:39 by pholster      ########   odam.nl         */
+/*   Updated: 2019/02/19 09:33:14 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** int		ft_getchar(void)
+** {
+** 	static int	pointer;
+** 	static char	*str = NULL;
+**
+** 	if (str == NULL)
+** 		get_next_line(1, &str);
+** 	else if (str[pointer] == '\0')
+** 	{
+** 		ft_strdel(&str);
+** 		pointer = 0;
+** 		return ('\n');
+** 	}
+** 	else
+** 		pointer++;
+** 	return (str[pointer]);
+** }
+*/
 
 int		ft_getchar(void)
 {
@@ -21,23 +41,3 @@ int		ft_getchar(void)
 		str[0] = '\n';
 	return (str[0]);
 }
-
-/*
-int		ft_getchar(void)
-{
-	static int	pointer;
-	static char	*str = NULL;
-
-	if (str == NULL)
-		get_next_line(1, &str);
-	else if (str[pointer] == '\0')
-	{
-		ft_strdel(&str);
-		pointer = 0;
-		return ('\n');
-	}
-	else
-		pointer++;
-	return (str[pointer]);
-}
-*/
