28c612ffa653a443b359d97edc9656d8546011aa
pholster <pholster@student.codam.nl>
2019-03-23 11:54:14 +0100

    made strarrcpy_var close ba args befor retruning on null

--- ft_strarrcpy_var.c
+++ ft_strarrcpy_var.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/18 16:18:39 by pholster       #+#    #+#                */
-/*   Updated: 2019/03/23 00:33:49 by pholster      ########   odam.nl         */
+/*   Updated: 2019/03/23 11:53:51 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ char	**ft_strarrcpy_var(char **dst, int count, ...)
 	{
 		dst[i] = ft_strdup(va_arg(args, char *));
 		if (dst[i] == NULL)
+		{
 			return (NULL);
+			va_end(args);
+		}
 		i++;
 	}
 	va_end(args);
