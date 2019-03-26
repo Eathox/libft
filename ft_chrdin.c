7080b8549046c034b157cc7e4501298f3abc9af4
pholster <pholster@student.codam.nl>
2019-03-26 18:38:39 +0100

    WIP CHRDIN

--- ft_chrdin.c
+++ ft_chrdin.c
@@ -6,13 +6,34 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/09 17:42:01 by pholster       #+#    #+#                */
-/*   Updated: 2019/02/24 00:00:34 by pholster      ########   odam.nl         */
+/*   Updated: 2019/03/26 18:19:17 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_chrdin(const char *str, const char *dlm)
+int			ft_chrdin(const char *str, const char *dlm)
 {
-	return (ft_chrdindex(str, dlm) != -1);
+	size_t		i;
+	long long	chars[2];
+
+	i = 0;
+	(void)str;
+	ft_memset(chars, 0, sizeof(chars));
+	while (dlm[i] != '\0')
+	{
+		chars[0] |= (1 << dlm[i]);
+		i++;
+	}
+	ft_putendl("");
+	ft_putbytes(&chars[1], sizeof(chars[1]));
+	ft_putendl("");
+	ft_putbytes(&chars[0], sizeof(chars[0]));
+	ft_putendl("");
+	return (FALSE);
 }
+
+// int			ft_chrdin(const char *str, const char *dlm)
+// {
+// 	return (ft_chrdindex(str, dlm) != -1);
+// }
--- ft_putbytes.c
+++ ft_putbytes.c
@@ -6,25 +6,24 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/26 16:13:31 by pholster       #+#    #+#                */
-/*   Updated: 2019/03/26 18:08:41 by pholster      ########   odam.nl         */
+/*   Updated: 2019/03/26 18:38:16 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putbytes(void *bytes, size_t n)
+void		ft_putbytes(void *bytes, size_t n)
 {
-	size_t		i;
-	long long	byt;
+	size_t	i;
+	char	*byts;
 
+	byts = (char *)bytes;
 	while (n > 0)
 	{
 		i = 8;
-		byt = *((long long *)bytes);
-		byt >>= 8 * (n - 1);
 		while (i > 0)
 		{
-			if ((byt >> (i - 1)) & 1)
+			if (byts[n - 1] & (1 << (i - 1)))
 				ft_putchar('1');
 			else
 				ft_putchar('0');
