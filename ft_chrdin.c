af30cf6a911170d75bc13d2f23bf59b56c7d2638
Eathox <Rendier3636@gmail.com>
2019-03-27 01:16:25 +0100

    optimized chrdindex

--- ft_chrdin.c
+++ ft_chrdin.c
@@ -12,29 +12,7 @@
 
 #include "libft.h"
 
-int			ft_chrdin(const char *str, const char *dlm)
+int		ft_chrdin(const char *str, const char *dlm)
 {
-	size_t		i;
-	long long	chars[2];
-
-	i = 0;
-	ft_memset(chars, 0, sizeof(chars));
-	while (dlm[i] != '\0')
-	{
-		if (dlm[i] >= 64)
-			chars[1] |= ((long long)1 << (dlm[i] - 64));
-		else
-			chars[0] |= ((long long)1 << dlm[i]);
-		i++;
-	}
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 64 && chars[1] & ((long long)1 << (str[i] - 64)))
-			return (TRUE);
-		if (chars[0] & ((long long)1 << str[i]))
-			return (TRUE);
-		i++;
-	}
-	return (FALSE);
+	return (ft_chrdindex(str, dlm) != -1);
 }
--- ft_chrdindex.c
+++ ft_chrdindex.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/02/03 22:38:37 by pholster       #+#    #+#                */
-/*   Updated: 2019/02/25 13:06:41 by pholster      ########   odam.nl         */
+/*   Updated: 2019/03/27 01:12:50 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,26 @@
 
 int		ft_chrdindex(const char *str, const char *dlm)
 {
-	size_t	i;
-	int		dex;
+	size_t		i;
+	long long	chars[2];
 
 	i = 0;
-	if (str == NULL || dlm == NULL)
-		return (-1);
+	ft_memset(chars, 0, sizeof(chars));
 	while (dlm[i] != '\0')
 	{
-		dex = ft_chrindex(str, dlm[i]);
-		if (dex != -1)
-			return (dex);
+		if (dlm[i] >= 64)
+			chars[1] |= ((long long)1 << (dlm[i] - 64));
+		else
+			chars[0] |= ((long long)1 << dlm[i]);
+		i++;
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 64 && chars[1] & ((long long)1 << (str[i] - 64)))
+			return (i);
+		if (chars[0] & ((long long)1 << str[i]))
+			return (i);
 		i++;
 	}
 	return (-1);
