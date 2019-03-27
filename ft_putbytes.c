4e41115b6ed5dd9b0302ab0edcf2d144c8b6eb03
pholster <pholster@student.codam.nl>
2019-03-27 16:30:05 +0100

    reveserd the printing of bytes

--- ft_putbytes.c
+++ ft_putbytes.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/26 16:13:31 by pholster       #+#    #+#                */
-/*   Updated: 2019/03/26 18:40:27 by pholster      ########   odam.nl         */
+/*   Updated: 2019/03/27 16:27:42 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,23 @@
 void		ft_putbytes(void *bytes, size_t n)
 {
 	size_t	i;
+	size_t	byt;
 	char	*byts;
 
+	byt = 0;
 	byts = (char *)bytes;
-	while (n > 0)
+	while (byt < n)
 	{
 		i = 8;
 		while (i > 0)
 		{
-			if (byts[n - 1] & ((long long)1 << (i - 1)))
+			if (byts[byt] & ((long long)1 << (i - 1)))
 				ft_putchar('1');
 			else
 				ft_putchar('0');
 			i--;
 		}
-		n--;
+		byt++;
 		if (n != 0)
 			ft_putchar(' ');
 	}
