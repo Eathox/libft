5a0b77d321aee8c0a91abd336ff30502488ccfe4
pholster <pholster@student.codam.nl>
2019-03-27 14:12:55 +0100

    made strdsplit norm compliant

--- ft_strdsplit.c
+++ ft_strdsplit.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/11 15:22:38 by pholster       #+#    #+#                */
-/*   Updated: 2019/03/27 02:57:10 by pholster      ########   odam.nl         */
+/*   Updated: 2019/03/27 14:11:49 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,15 @@ char		**ft_strdsplit(const char *str, const char *dlm)
 	ft_chrsetbytes(bytes, (char *)dlm);
 	while (str[i] != '\0')
 	{
-		if (ft_chrinbytes(bytes, str[i]) == FALSE)
-		{
-			len = ft_strdlen(&str[i], dlm);
-			arr[word] = ft_strndup(&str[i], len);
-			if (arr[word] == NULL)
-				return (freeret(&arr));
-			i += (len - 1);
-			word++;
-		}
 		i++;
+		if (ft_chrinbytes(bytes, str[i - 1]))
+			continue ;
+		len = ft_strdlen(&str[i - 1], dlm);
+		arr[word] = ft_strndup(&str[i - 1], len);
+		if (arr[word] == NULL)
+			return (freeret(&arr));
+		i += (len - 1);
+		word++;
 	}
 	return (arr);
 }
