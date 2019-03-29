dab4524dd632140b37fff7d53c82dfd2db5351e6
Eathox <Rendier3636@gmail.com>
2019-03-29 23:49:39 +0100

    updated rounding once more

--- ft_putdouble.c
+++ ft_putdouble.c
@@ -12,58 +12,37 @@
 
 #include "libft.h"
 
-// static void	putprecision(long double value, size_t n)
-// {
-// 	size_t		i;
-// 	int			num;
-// 	long double	next;
-// 	long double	nextnext;
-// 	long double	nextnextnext;
-
-// 	i = 0;
-// 	while (i < n)
-// 	{
-// 		value *= 10;
-// 		num = (int)value;
-// 		next = (value - num) * 10;
-// 		nextnext = (next - (int)next) * 10;
-// 		nextnextnext = (nextnext - (int)nextnext) * 10;
-// 		if (num < 9)
-// 		{
-// 			if ((int)next == 9 && (int)nextnext >= 8 && (int)nextnextnext >= 7)
-// 				num++;
-// 			else if ((int)next >= 5 && (i + 1) == n)
-// 				num++;
-// 		}
-// 		ft_putnbr(num);
-// 		value -= num;
-// 		i++;
-// 	}
-// }
-
-// max precision of char 24 and use this
-
-static int	putprecision(long double value, size_t n)
+static void	adddecimals(long double value, char *decimals, size_t n)
 {
-	int			num;
-	int			rnd;
-	long double	next;
+	size_t	i;
+	int		rnd;
+	int		num;
 
-	num = (int)value;
-	next = (value - num) * 10;
-	if (n == 1)
-		rnd = ((int)next >= 9);
-	else
-		rnd = putprecision(next, n - 1);
-	num += rnd;
-	if (num == 10)
-		num = 0;
-	ft_putnbr(num);
-	return (num == 9);
+	i = 0;
+	while (i < (n + 1))
+	{
+		value *= 10;
+		num = (int)value;
+		decimals[i] = num + '0';
+		value -= num;
+		i++;
+	}
+	i--;
+	rnd = (num >= 5);
+	while (i > 0)
+	{
+		i--;
+		decimals[i] += rnd;
+		if (decimals[i] == ('9' + 1))
+			decimals[i] = '0';
+		else
+			rnd = 0;
+	}
 }
 
 void		ft_putdouble(long double value, size_t n)
 {
+	char		decimals[25];
 	long long	num;
 
 	if (value < 0)
@@ -72,11 +51,11 @@ void		ft_putdouble(long double value, size_t n)
 		value *= -1;
 	}
 	num = (long long)value;
-	ft_putnbr(num);
+	value -= num;
+	adddecimals(value, decimals, n);
+	ft_putnbr(num + (n == 0 && decimals[0] >= '5'));
 	if (n == 0)
 		return ;
 	ft_putchar('.');
-	value -= num;
-	value *= 10;
-	putprecision(value, n);
+	ft_putnstr(decimals, n);
 }
