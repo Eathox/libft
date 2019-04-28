8410f800bb7fc94c2d396b32399616a85947bae9
Eathox <Rendier3636@gmail.com>
2019-04-28 02:06:44 +0200

    Fixed pf_commands

--- ft_printf/pf_commands.c
+++ ft_printf/pf_commands.c
@@ -6,38 +6,33 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/18 15:47:54 by pholster       #+#    #+#                */
-/*   Updated: 2019/04/01 13:28:03 by pholster      ########   odam.nl         */
+/*   Updated: 2019/04/28 02:05:05 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-// int		pf_commands(t_info *info, const char *str)
-// {
-// 	int	len;
-
-// 	len = ft_strislen(str, &ft_isdigit);
-// 	if (*str == ']')
-// 	{
-// 		ft_termcommand_fd(0, PF_FD);
-// 		return (2);
-// 	}
-// 	if (ft_strnequ_nocase(str, "clr]", 4))
-// 	{
-// 		ft_termclr_fd(PF_FD);
-// 		return (5);
-// 	}
-// 	if (len != 0 && str[len] == ']')
-// 	{
-// 		ft_termcommand_fd(ft_atoi(str), PF_FD);
-// 		return (2 + len);
-// 	}
-// 	return (0);
-// }
-
 int		pf_commands(t_info *info, const char *str)
 {
-	(void)info;
-	(void)str;
+	int	len;
+
+	len = ft_strislen(str, &ft_isdigit);
+	if (*str == ']')
+	{
+		pf_addtobuff(info, "\e[0m", 5);
+		return (2);
+	}
+	if (ft_strnequ_nocase(str, "clr]", 4))
+	{
+		pf_addtobuff(info, "\e[1;1H\e[2J\n", 12);
+		return (5);
+	}
+	if (len != 0 && str[len] == ']')
+	{
+		pf_addtobuff(info, "\e[", 3);
+		pf_addtobuff(info, str, len);
+		pf_addtobuff(info, "m", 1);
+		return (2 + len);
+	}
 	return (0);
 }
--- ft_printf/pf_distribute.c
+++ ft_printf/pf_distribute.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/01 13:25:57 by pholster       #+#    #+#                */
-/*   Updated: 2019/04/01 13:27:41 by pholster      ########   odam.nl         */
+/*   Updated: 2019/04/28 02:06:02 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ int		pf_distribute(t_info *info, const char *start)
 {
 	int	i;
 
-	// if (*start == '{')
-	// 	i = pf_setcolor(info, &start[1]); //WIP
-	// else if (*start == '[')
-	// 	i = pf_commands(info, &start[1]); //WIP
-	// else
-	// {
+	if (*start == '{')
+		i = pf_setcolor(info, &start[1]);
+	else if (*start == '[')
+		i = pf_commands(info, &start[1]);
+	else
+	{
 		pf_infosetdefualt(info);
 		i = pf_getinfo(info, start);
 		pf_format(info);
-	// }
+	}
 	return (i);
 }
