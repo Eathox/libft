951ee19025244ded3e01676757a55b4bb96d2aa4
pholster <pholster@student.codam.nl>
2019-04-29 17:19:51 +0200

    changed add unum a bit

--- ft_printf/pf_addunum.c
+++ ft_printf/pf_addunum.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/12 02:16:09 by pholster       #+#    #+#                */
-/*   Updated: 2019/04/29 16:04:18 by pholster      ########   odam.nl         */
+/*   Updated: 2019/04/29 17:19:17 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,17 @@
 void	pf_addunum(t_info *info, t_uintmax value, int len, int base)
 {
 	char	c;
-	char	*base_str;
 
 	if (len == 0)
 		return ;
-	if (PF_TYPE == 'X')
-		base_str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	else
-		base_str = "0123456789abcdefghijklmnopqrstuvwxyz";
 	if (len > 1)
 		pf_addunum(info, value / base, len - 1, base);
-	c = base_str[(value % base)];
+	c = value % base;
+	if (c <= 9)
+		c += '0';
+	else if (c > 9 && PF_TYPE != 'X')
+		c += 'a' - 10;
+	else
+		c += 'A' - 10;
 	pf_addtobuff(info, &c, 1);
 }
