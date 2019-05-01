7579864c2da1f85e008c6f546afc191a053d21b2
Eathox <Rendier3636@gmail.com>
2019-05-01 12:44:31 +0200

    memrcpy

--- ft_memrcpy.c
+++ ft_memrcpy.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/11 16:56:29 by pholster       #+#    #+#                */
-/*   Updated: 2019/04/30 18:40:36 by pholster      ########   odam.nl         */
+/*   Updated: 2019/05/01 12:42:37 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void	*ft_memrcpy(void *dst, const void *src, size_t len)
 	i = (len / 8);
 	temp_dst = dst;
 	temp_src = src;
-	while (len >= 8)
+	while (len - (i * 8) >= 8)
 	{
 		i--;
-		len -= 8;
 		((long long *)temp_dst)[i] = ((long long *)temp_src)[i];
 	}
+	len -= (i * 8);
 	while (len > 0)
 	{
 		len--;
