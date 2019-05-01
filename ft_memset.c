84b62b923d1d7248b0fcd57e79029323574ad8a0
Eathox <Rendier3636@gmail.com>
2019-05-01 03:01:06 +0200

    expirmental optimaztion to memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/11 16:08:38 by pholster       #+#    #+#                */
-/*   Updated: 2019/04/30 18:50:04 by pholster      ########   odam.nl         */
+/*   Updated: 2019/05/01 02:59:44 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,24 @@
 void	*ft_memset(void *str, int c, size_t len)
 {
 	size_t			i;
+	long long		temp_c;
 	char			*temp_str;
 
 	i = 0;
+	temp_c = 0;
 	temp_str = str;
+	while (i < 8)
+	{
+		temp_c |= (((long long)(unsigned char)c) << (i * 8));
+		i++;
+	}
+	i = 0;
+	while (len - (i * 8) >= 8)
+	{
+		((long long *)temp_str)[i] = temp_c;
+		i++;
+	}
+	i *= 8;
 	while (i < len)
 	{
 		temp_str[i] = (unsigned char)c;
--- ft_putbytes.c
+++ ft_putbytes.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/26 16:13:31 by pholster       #+#    #+#                */
-/*   Updated: 2019/03/29 12:39:22 by pholster      ########   odam.nl         */
+/*   Updated: 2019/05/01 02:58:18 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 void		ft_putbytes(void *bytes, size_t n)
 {
-	size_t	i;
 	char	*byts;
 
 	byts = (char *)bytes;
 	while (n > 0)
 	{
-		i = 8;
-		while (i > 0)
-		{
-			if (byts[n - 1] & (1LL << (i - 1)))
-				ft_putchar('1');
-			else
-				ft_putchar('0');
-			i--;
-		}
 		n--;
+		ft_putchar(((byts[n] & (1LL << 0)) != 0) + '0');
+		ft_putchar(((byts[n] & (1LL << 1)) != 0) + '0');
+		ft_putchar(((byts[n] & (1LL << 2)) != 0) + '0');
+		ft_putchar(((byts[n] & (1LL << 3)) != 0) + '0');
+		ft_putchar(((byts[n] & (1LL << 4)) != 0) + '0');
+		ft_putchar(((byts[n] & (1LL << 5)) != 0) + '0');
+		ft_putchar(((byts[n] & (1LL << 6)) != 0) + '0');
+		ft_putchar(((byts[n] & (1LL << 7)) != 0) + '0');
 		if (n != 0)
 			ft_putchar(' ');
 	}
