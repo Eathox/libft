7c273fe0a9426102f4cd51b5efd5cd35ee337497
pholster <pholster@student.codam.nl>
2019-05-02 19:31:24 +0200

    fixed memrcpy

--- ft_memrcpy.c
+++ ft_memrcpy.c
@@ -6,27 +6,23 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/11 16:56:29 by pholster       #+#    #+#                */
-/*   Updated: 2019/05/01 15:12:47 by pholster      ########   odam.nl         */
+/*   Updated: 2019/05/02 19:31:07 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
 
+/*
+** make it use 8 by 8 long long cpy
+*/
+
 void	*ft_memrcpy(void *dst, const void *src, size_t len)
 {
-	size_t				i;
 	unsigned char		*temp_dst;
 	unsigned const char	*temp_src;
 
-	i = (len / 8);
 	temp_dst = dst;
 	temp_src = src;
-	while (len >= 8)
-	{
-		i--;
-		len -= 8;
-		((long long *)temp_dst)[i] = ((long long *)temp_src)[i];
-	}
 	while (len > 0)
 	{
 		len--;
