5c87ed6d7058817594ff48765d937c9e4ea2e862
pholster <pholster@student.codam.nl>
2019-06-27 17:48:01 +0200

    made lstdel be able to only del the list and not the content

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/11 18:35:27 by pholster       #+#    #+#                */
-/*   Updated: 2019/03/16 22:53:50 by pholster      ########   odam.nl         */
+/*   Updated: 2019/06/27 17:47:25 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstdel(t_list **head, void (*del)(void *, size_t))
 {
-	if (head == NULL || *head == NULL || del == NULL)
+	if (head == NULL || *head == NULL)
 		return ;
 	if ((*head)->next != NULL)
 		ft_lstdel(&(*head)->next, del);
