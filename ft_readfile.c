2b26daa88b600ba21599713987aeb3ed4c0e26d8
Eathox <Rendier3636@gmail.com>
2019-07-05 12:03:46 +0200

    optimized readfile

--- ft_readfile.c
+++ ft_readfile.c
@@ -6,33 +6,82 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/03 18:30:38 by pholster       #+#    #+#                */
-/*   Updated: 2019/06/05 12:47:19 by pholster      ########   odam.nl         */
+/*   Updated: 2019/07/05 12:02:41 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
 
-char	*ft_readfile(int fd)
+static char	*freeret(t_list *lst)
 {
-	char	*str;
-	char	buff[BUFF_SIZE + 1];
-	int		ret;
+	ft_lstdel(&lst, &ft_lstdelmem);
+	return (NULL);
+}
 
-	str = NULL;
-	ft_bzero(buff, BUFF_SIZE + 1);
-	ret = (int)read(fd, buff, BUFF_SIZE);
-	while (ret >= 0)
+static char	*lsttostr(t_list *lst, size_t tottallen)
+{
+	size_t	i;
+	t_list	*next;
+	char	*str;
+
+	i = 0;
+	if (lst == NULL)
+		return (ft_strnew(0));
+	str = ft_strnew(tottallen);
+	if (str == NULL)
+		return (NULL);
+	while (lst != NULL)
 	{
-		if (str == NULL)
-			str = ft_strdup(buff);
-		else
-			ft_strappend(&str, buff);
-		if (str == NULL)
-			return (NULL);
-		if (ret == 0)
-			break ;
-		ft_strclr(buff);
-		ret = (int)read(fd, buff, BUFF_SIZE);
+		if (lst->content != NULL)
+			ft_memcpy(&str[i], lst->content, lst->content_size);
+		i += lst->content_size;
+		next = lst->next;
+		free(lst);
+		lst = next;
 	}
 	return (str);
+}
+
+static int	addtolst(t_list **lst, t_list **prv, char *str, size_t len)
+{
+	t_list	*new;
+
+	new = ft_lstnew((const void *)str, len);
+	if (new == NULL)
+		return (FALSE);
+	if (*lst == NULL)
+		*lst = new;
+	else
+		(*prv)->next = new;
+	*prv = new;
+	return (TRUE);
+}
+
+char		*ft_readfile(int fd)
+{
+	t_list	*prv;
+	t_list	*lst;
+	ssize_t	ret;
+	size_t	totallen;
+	char	buff[BUFF_SIZE + 1];
+
+	lst = NULL;
+	prv = NULL;
+	totallen = 0;
+	ret = read(fd, buff, BUFF_SIZE);
+	if (ret == -1)
+		return (NULL);
+	while (ret > 0)
+	{
+		totallen += ret;
+		buff[ret] = '\0';
+		if (addtolst(&lst, &prv, buff, ret) == FALSE)
+			return (freeret(lst));
+		if (ret == 0)
+			break ;
+		ret = read(fd, buff, BUFF_SIZE);
+		if (ret == -1)
+			return (freeret(lst));
+	}
+	return (lsttostr(lst, totallen));
 }
