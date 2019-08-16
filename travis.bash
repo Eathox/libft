efad3a14efb2b3f546fe109580edf2965998f7bc
pholster <pholster@student.codam.nl>
2019-08-16 14:49:30 +0200

    automatic confimration with sudo

--- travis.bash
+++ travis.bash
@@ -3,9 +3,9 @@
 if [ -z "$TRAVIS" ] || [ $TRAVIS != true ]; then
 	echo "Error: Not running travis"
 elif [ $TRAVIS_OS_NAME == "linux" ]; then
-	add-apt-repository ppa:snaipewastaken/ppa
-	apt-get update
-	apt-get install criterion-dev
+	sudo add-apt-repository -y ppa:snaipewastaken/ppa
+	sudo apt-get update
+	sudo apt-get install -y criterion-dev
 	make test
 elif [ $TRAVIS_OS_NAME == "osx" ]; then
 	export HOMEBREW_NO_AUTO_UPDATE=1
