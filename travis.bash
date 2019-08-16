e86ec9097ba2480112bbbe18e53de2952d88a005
pholster <pholster@student.codam.nl>
2019-08-16 12:50:55 +0200

    travis fix make and linux dectection

--- travis.bash
+++ travis.bash
@@ -2,15 +2,15 @@
 
 if [ -z "$TRAVIS" ] || [ $TRAVIS != true ]; then
 	echo "Error: Not running travis"
-elif [ $TRAVIS_OS_NAME == "Linux" ]; then
+elif [ $TRAVIS_OS_NAME == "linux" ]; then
 	sudo add-apt-repository ppa:snaipewastaken/ppa
 	sudo apt-get update
 	sudo apt-get install criterion-dev
-	make -C ../ test
+	make test
 elif [ $TRAVIS_OS_NAME == "osx" ]; then
 	export HOMEBREW_NO_AUTO_UPDATE=1
 	brew install snaipe/soft/criterion
-	make -C ../ test
+	make test
 else
 	echo "Error: OS is not supported"
 fi
