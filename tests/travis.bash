ee7758af29c31e428cc2327d3c373984115dbe2d
pholster <pholster@student.codam.nl>
2019-08-16 12:00:43 +0200

    travis ground work

---
+++ tests/travis.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -n "$TRAVIS"]; then
+	echo "Error: Not running travis"
+elif [ $TRAVIS_OS_NAME == "Linux" ]; then
+	sudo add-apt-repository ppa:snaipewastaken/ppa
+	sudo apt-get update
+	sudo apt-get install criterion-dev
+	make -C ../ test
+elif [ $TRAVIS_OS_NAME == "osx" ]; then
+	export HOMEBREW_NO_AUTO_UPDATE=1
+	brew install snaipe/soft/criterion
+	make -C ../ test
+else
+	echo "Error: Bios is not supported"
+fi
