e429abc24148cce3340689a7e97c0e85ba0310b0
pholster <pholster@student.codam.nl>
2019-08-18 19:53:08 +0200

    fixed bzero to ft_bzero

--- tests/test_ft_bzero.c
+++ tests/test_ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/17 15:59:22 by pholster       #+#    #+#                */
-/*   Updated: 2019/08/18 14:28:58 by pholster      ########   odam.nl         */
+/*   Updated: 2019/08/18 19:52:49 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ Test(bzero, str_term) {
 	char	tar[] = "\0\0\0\0\0bar";
 	char	str[] = "foo\0 bar";
 
-	bzero(str, 5);
+	ft_bzero(str, 5);
 	cr_assert_arr_eq(str, tar, sizeof(tar));
 }
 
 Test(bzero, num_half) {
-	int		num;
+	unsigned int	num;
 
 	num = 0xFFFFFFFF;
 	ft_bzero(&num, 2);
