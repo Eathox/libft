5c06b33bc01476d819d77a2f42316c92dd23bb1a
pholster <pholster@student.codam.nl>
2019-08-23 12:31:14 +0200

    more hash tests

--- tests/src/test_ft_hashmapadd.c
+++ tests/src/test_ft_hashmapadd.c
@@ -6,7 +6,7 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/19 16:57:19 by pholster       #+#    #+#                */
-/*   Updated: 2019/08/19 19:21:07 by pholster      ########   odam.nl         */
+/*   Updated: 2019/08/23 12:30:43 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,22 @@ Test(hashmapadd, key_null, .signal = SIGSEGV) {
 
 	tab = ft_hashmapnew(5, &ft_hashsdbm);
 	cr_assert(ft_hashmapadd(tab, NULL) == FALSE);
+}
+
+Test(hashmapadd, content_null, .signal = SIGSEGV) {
+	cr_assert(ft_hashmapadd(NULL, "hashmap_null") == FALSE);
+}
+
+Test(hashmapadd, contentsize_null, .signal = SIGSEGV) {
+	t_hashmap	*tab;
+
+	tab = ft_hashmapnew(5, &ft_hashsdbm);
+	cr_assert(ft_hashmapadd(tab, NULL) == FALSE);
+}
+
+Test(hashmapadd, add1) {
+	t_hashmap	*tab;
+
+	tab = ft_hashmapnew(1, &ft_hashsdbm);
+	cr_assert(ft_hashmapadd(tab, "Key_1") == FALSE);
 }
