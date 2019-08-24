a25c5c2ea3fd58ddcf64025b7d9c1ebeb95878a5
pholster <pholster@student.codam.nl>
2019-08-24 17:22:58 +0200

    made list use free instead of memdel

--- src/list/ft_lstdelmem.c
+++ src/list/ft_lstdelmem.c
@@ -6,15 +6,15 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/13 15:09:24 by pholster       #+#    #+#                */
-/*   Updated: 2019/08/21 21:08:20 by pholster      ########   odam.nl         */
+/*   Updated: 2019/08/24 17:08:00 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
-#include "ft_memory.h"
+#include <stdlib.h>
 
 void	ft_lstdelmem(void *content, size_t size)
 {
 	(void)size;
-	ft_memdel(&content);
+	free(content);
 }
