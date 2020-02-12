46d18bd86fb464154b861619d06f109f52b6d321
pholster <pholster@student.codam.nl>
2020-02-12 15:40:31 +0100

    fix lstunlink if target is head lst will be lost

--- src/list/ft_lstunlink.c
+++ src/list/ft_lstunlink.c
@@ -16,14 +16,16 @@ t_list	*ft_lstunlink(t_list **head, t_list *lst)
 {
 	t_list	*current;
 
+	if (*head == lst)
+	{
+		*head = lst->next;
+		return (lst);
+	}
 	current = *head;
-	while (current != NULL && current != lst && current->next != lst)
+	while (current != NULL && current->next != lst)
 		current = current->next;
 	if (current == NULL)
 		return (NULL);
-	if (current == *head)
-		*head = lst->next;
-	else
-		current->next = lst->next;
+	current->next = lst->next;
 	return (lst);
 }
