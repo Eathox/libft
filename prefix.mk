1580da7f8f752e5926a88e8a1be4bb291a239e59
Eahox <rendier3636@gmail.com>
2020-02-17 19:52:39 +0100

    Re arange prefix.mk

--- prefix.mk
+++ prefix.mk
@@ -10,7 +10,8 @@
 #                                                                              #
 # **************************************************************************** #
 
-all-public-headers += $(public-headers)
 $(module)-private-headers := $(addprefix $(module)/,$(private-headers))
+all-public-headers += $(public-headers)
+
 all-objects += $(addprefix $(module)/,$(objects))
 all-tests += $(addprefix $(module)/,$(tests))
