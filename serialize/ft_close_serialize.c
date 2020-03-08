eb1b37321b58fe45ff30020e37c89431c1a5fde8
Eahox <rendier3636@gmail.com>
2020-03-08 14:54:01 +0100

    Serialize close, calls clear

--- serialize/ft_close_serialize.c
+++ serialize/ft_close_serialize.c
@@ -20,6 +20,7 @@ t_bool		ft_close_serialize(t_serialize *serialize)
 	int		ret;
 
 	ret = close(serialize->fd);
+	ft_clear_serialize(serialize);
 	serialize->fd = DEFAULT_FD;
 	serialize->pos = 0;
 	if (ret < 0)
