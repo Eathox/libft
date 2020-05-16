fa166edae8bdec13f1b9a2bc6f444732df8fcb36
Eahox <rendier3636@gmail.com>
2020-05-16 21:03:31 +0200

    Simple types size tests

---
+++ src/types/types_test.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   types_test.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pholster <pholster@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/05/16 20:55:42 by pholster      #+#    #+#                 */
+/*   Updated: 2020/05/16 20:55:42 by pholster      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <criterion/criterion.h>
+
+#include "types.h"
+
+Test(types, t_int8_size)
+{
+	cr_assert_eq(sizeof(t_int8), 1);
+}
+
+Test(types, t_int16_size)
+{
+	cr_assert_eq(sizeof(t_int16), 2);
+}
+
+Test(types, t_int32_size)
+{
+	cr_assert_eq(sizeof(t_int32), 4);
+}
+
+Test(types, t_int64_size)
+{
+	cr_assert_eq(sizeof(t_int64), 8);
+}
+
+Test(types, t_uint8_size)
+{
+	cr_assert_eq(sizeof(t_uint8), 1);
+}
+
+Test(types, t_uint16_size)
+{
+	cr_assert_eq(sizeof(t_uint16), 2);
+}
+
+Test(types, t_uint32_size)
+{
+	cr_assert_eq(sizeof(t_uint32), 4);
+}
+
+Test(types, t_uint64_size)
+{
+	cr_assert_eq(sizeof(t_uint64), 8);
+}
