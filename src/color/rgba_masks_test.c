9f2585129fad553b3c89025cb5e3e9de9a5c2198
Eahox <rendier3636@gmail.com>
2020-05-18 04:11:47 +0200

    remvoed () around bit shift for consistancy

--- src/color/rgba_masks_test.c
+++ src/color/rgba_masks_test.c
@@ -26,7 +26,7 @@ Test(rgba_masks_test, order)
 
 	rgba = ft_convert_color_to_rgba(&color);
 	cr_assert_eq((rgba & RGBA_RED_MASK), 0x01 << RGBA_RED_SHIFT);
-	cr_assert_eq((rgba & RGBA_GREEN_MASK), (0x02 << RGBA_GREEN_SHIFT));
-	cr_assert_eq((rgba & RGBA_BLUE_MASK), (0x03 << RGBA_BLUE_SHIFT));
-	cr_assert_eq((rgba & RGBA_ALPHA_MASK), (0x04 << RGBA_ALPHA_SHIFT));
+	cr_assert_eq((rgba & RGBA_GREEN_MASK), 0x02 << RGBA_GREEN_SHIFT);
+	cr_assert_eq((rgba & RGBA_BLUE_MASK), 0x03 << RGBA_BLUE_SHIFT);
+	cr_assert_eq((rgba & RGBA_ALPHA_MASK), 0x04 << RGBA_ALPHA_SHIFT);
 }
