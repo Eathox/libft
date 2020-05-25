7f0a8a6edf8c9be3d02e073b2b230145b38ec280
Eahox <rendier3636@gmail.com>
2020-05-25 20:30:58 +0200

    cleanup normify a bunch

--- tools/normify.py
+++ tools/normify.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+# **************************************************************************** #
+#                                                                              #
+#                                                         ::::::::             #
+#    normify.py                                         :+:    :+:             #
+#                                                      +:+                     #
+#    By: pholster <pholster@student.codam.nl>         +#+                      #
+#                                                    +#+                       #
+#    Created: 2020/05/25 19:03:41 by pholster      #+#    #+#                  #
+#    Updated: 2020/05/25 19:03:41 by pholster      ########   odam.nl          #
+#                                                                              #
+# **************************************************************************** #
 
 import re
 import os
@@ -7,14 +18,14 @@ import glob
 import shutil
 
 normDir = "norm"
+sourceDir = "src"
+targetDirectories = (
+	"makefile_mk",
+	sourceDir
+)
 targetFiles = (
 	"author",
 	"makefile"
-)
-targetDirectories = (
-	"makefile_mk",
-	# ".vscode",
-	"src"
 )
 
 # --- Create the normDir and copy over important files
@@ -28,19 +39,19 @@ for directory in targetDirectories:
 	try:
 		shutil.copytree(directory, os.path.join(normDir, directory))
 	except:
-		print(f"Error copying directory: '{directory}'")
+		print(f"Error: Copying directory '{directory}'")
 
 for file in targetFiles:
 	try:
 		shutil.copy(file, os.path.join(normDir, file))
 	except:
-		print(f"Error copying file: '{file}'")
+		print(f"Error: Copying file '{file}'")
 
 # --- Replace file content function
 def replaceContent(file, findRegex, replaceRegex):
 	with open(file, "r") as fd:
 		content = fd.read()
-	content = re.sub(findRegex, replaceRegex, content)
+	content = re.sub(findRegex, replaceRegex, content, flags=re.MULTILINE)
 	with open(file, "w") as fd:
 		fd.write(content)
 
@@ -48,26 +59,29 @@ def replaceContent(file, findRegex, replaceRegex):
 normPath = os.path.join(targetPath, normDir)
 os.chdir(normPath)
 
-for file in glob.glob("src/**/*_test.c", recursive=True):
+globPattern = os.path.join(sourceDir, "**/*_test.c")
+for file in glob.glob(globPattern, recursive=True):
 	try:
 		os.remove(file)
 	except:
-		print(f"Error removing file: '{file}'")
+		print(f"Error: Removing file '{file}'")
 
 # Add newline between function prototype and quick info
-for file in glob.glob("src/**/*.c", recursive=True):
-	quickInfoRegex = r"(\/\*\n(.|\n)*\n?\*\/)(\n.*\n?\()"
-	quickInfoReplace = r"\1\n\3"
+quickInfoRegex = r"(\/\*\n(\*{2}.*\n)*\*\/)(\n\s{0}\S+\s.*?\n?\()"
+quickInfoReplace = r"\1\n\3"
+globPattern = os.path.join(sourceDir, "**/*.c")
+for file in glob.glob(globPattern, recursive=True):
 	try:
 		replaceContent(file, quickInfoRegex, quickInfoReplace)
 	except:
-		print(f"Error replacing content of file: '{file}'")
+		print(f"Error: Replacing content of file '{file}'")
 
 # Remove all test makefile targets
-for file in glob.glob("src/**/make.mk", recursive=True):
-	testRulesRegex = r"\n*tests\s?\+=.*"
-	testRulesReplace = r""
+testRulesRegex = r"\n*tests\s?\+=.*"
+testRulesReplace = r""
+globPattern = os.path.join(sourceDir, "**/make.mk")
+for file in glob.glob(globPattern, recursive=True):
 	try:
 		replaceContent(file, testRulesRegex, testRulesReplace)
 	except:
-		print(f"Error replacing content of file: '{file}'")
+		print(f"Error: Replacing content of file '{file}'")
