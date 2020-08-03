2cbb0c43d64f1aed12265a1e7f04ba4b87458f5f
Eathox <Rendier3636@gmail.com>
2020-08-03 23:50:04 +0200

    adding argparse to quickInfo.py

--- tools/quickInfo.py
+++ tools/quickInfo.py
@@ -15,16 +15,26 @@ import re
 import os
 import sys
 import glob
+import pathlib
+import argparse
 
-# WIP Add argparser and make more robust
+# WIP make more robust
 
 sourceDir = "src"
+description="Checks and report's functions that are missing quick info"
 
 targetPath = os.path.dirname(sys.path[0]) # Removes '/tools' from the end
 os.chdir(targetPath)
 
+argParser = argparse.ArgumentParser(description=description)
+argParser.add_argument("directory", nargs="?",
+	default=sourceDir, type=pathlib.Path,
+	help="directory to search")
+
+args = argParser.parse_args()
+
 noQuickInfoRegex = r"^\n(?!static)\s{0}\S+\s.*?\n?\((.|\n)*\)\n"
-globPattern = os.path.join(sourceDir, "**/*[!_test].c")
+globPattern = os.path.join(args.directory, "**/*[!_test].c")
 for file in glob.glob(globPattern, recursive=True):
 	try:
 		with open(file, "r") as fd:
