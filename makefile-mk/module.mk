d1acd2277b8dbe5c1adc520c869be31cabeab685
Eathox <pgaholster@gmail.com>
2021-06-25 02:46:00 +0200

    Change regex echo to error msg in module.mk

--- makefile-mk/module.mk
+++ makefile-mk/module.mk
@@ -13,9 +13,9 @@ $(HEADER_PATH)/%.h: $(SRCDIR)/%/$(notdir $(module)).h
 # The header files are in the same directory as the source files. But as a
 # result the include paths are relative to how the src directory is structured
 # Once the header files get moved to the OUTDIR these paths are no longer valid
-# here for this regex replaces include statements formatted like "../name/name.h" to "name.h"
+# this regex replaces include statements formatted like "../name/name.h" to "name.h"
 ifeq ($(OS_NAME), Darwin)
-	@echo "Darwin regex not implemented"
+	$(error Darwin regex not implemented (headers not usable))
 else
 	@sed -Ei 's,\.\.\/(.*\/)?(\w*)\/\2\.h,\1\2\.h,g' $@
 endif
