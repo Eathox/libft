30ff7bfa84d91079cdfc048705efa0d882be239f
Eathox <pgaholster@gmail.com>
2021-10-19 20:33:17 +0200

    Remove norm dir exclusion from ci/gcov.bash

--- ci/gcov.bash
+++ ci/gcov.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-NORM_DIR="norm"
 BUILD_DIR="build"
 
-cpp-coveralls --gcov-options '\-lp' -b "." --exclude "$NORM_DIR" \
+cpp-coveralls --gcov-options '\-lp' -b "." \
 	--exclude "$BUILD_DIR/include" --exclude "$BUILD_DIR/cache/test"
