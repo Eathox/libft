b7b9aee1b60a895ad8e4782f41eb0d04e38cc8f4
Eathox <pgaholster@gmail.com>
2022-04-15 13:03:02 +0200

    Use libft abs_float

--- modules/math/float/src/ft_nearest_num_float.c
+++ modules/math/float/src/ft_nearest_num_float.c
@@ -8,8 +8,8 @@ float128_t ft_nearest_num_float(
 	float128_t num1,
 	float128_t num2
 ) {
-	float128_t const num1_distance = fabsl(num1 - value);
-	float128_t const num2_distance = fabsl(num2 - value);
+	float128_t const num1_distance = ft_abs_float(num1 - value);
+	float128_t const num2_distance = ft_abs_float(num2 - value);
 
 	if (num2_distance < num1_distance)
 		return (num2);
